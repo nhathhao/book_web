21ffc69ea1c85eddf3b917b07df09c0c678b425d
quydaa <manphan372@gmail.com>
2025-03-28 16:17:47 +0700

    Tải Laravel Breeze bằng lệnh: composer require laravel/breeze --dev; Cài đặt Laravel Breeze bằng các lệnh:  php artisan breeze:install;  npm install;  npm run dev; Tạo các table liên quan bằng lệnh: php artisan migrate fresh

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () 
-{
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/sach','App\Http\Controllers\LayoutController@sach');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/sach/theloai/{id}','App\Http\Controllers\LayoutController@theloai');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/sach/chitiet/{id}','App\Http\Controllers\LayoutController@chitiet');
-
+require __DIR__.'/auth.php';
