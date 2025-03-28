4dd20740f813f86d240c66b035066d5a58dc462d
quydaa <manphan372@gmail.com>
2025-03-28 10:04:51 +0700

    Lưu thay đổi trước khi chuyển sang nhánh manhdz

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', function () 
+{
     return view('welcome');
 });
 
@@ -11,3 +12,4 @@ Route::get('/sach','App\Http\Controllers\LayoutController@sach');
 Route::get('/sach/theloai/{id}','App\Http\Controllers\LayoutController@theloai');
 
 Route::get('/sach/chitiet/{id}','App\Http\Controllers\LayoutController@chitiet');
+
