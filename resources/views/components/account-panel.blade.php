238f91db02e91bac09b86faf48cefcf8c2c604f8
Nhat Hao <nhathao05104@gmail.com>
2025-04-12 01:26:39 +0700

    Update homepage link in account panel navigation

--- resources/views/components/account-panel.blade.php
+++ resources/views/components/account-panel.blade.php
@@ -62,7 +62,7 @@
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="{{url('sach')}}">Trang chủ</a>
+                <a class="nav-link" href="{{url('/')}}">Trang chủ</a>
             </li>
         </ul>
   </nav>
